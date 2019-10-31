75efb240dd714d63fcfa08709f77ff19d74dbd12
Anirudh Kumar <anirudh.anirudh.yadav85@gmail.com>
2019-10-31 15:24:16 +0530

    set permission script updated

--- setPermission.sh
+++ setPermission.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
-echo $2
-if [ $# -eq 2 ]; then
-    if [ -d "$2" ]; then
-        echo "Path passed through arg is DIRECTORY."
-        sudo chmod $1 -R $2
-        echo "Permission set successfully."
-    elif [ -f "$2" ]; then
-        echo "Path passed through arg is FILE."
-        sudo chmod $1 -R $2
-        echo "Permission set successfully."
+
+NUMBER_REGEX="^[0-9]{1,3}$"
+
+if [[ $1 =~ $NUMBER_REGEX ]]; then
+    if [ $# -eq 2 ]; then
+        if [ -d "$2" ]; then
+            echo "Path passed through arg is DIRECTORY."
+            sudo chmod $1 -R $2
+            echo "Permission set successfully."
+            exit 0
+        elif [ -f "$2" ]; then
+            echo "Path passed through arg is FILE."
+            sudo chmod $1 -R $2
+            echo "Permission set successfully."
+            exit 0
+        else
+            echo "Path doesn't exist."
+            exit 1
+        fi
     else
-        echo "Path doesn't exist."
+        echo "Invalid number of arguments."
+        echo "There must be 2 args."
+        echo "1. First one is permission code"
+        echo "2. File/Folder with complete path"
+        exit 0
     fi
 else
-    echo "Invalid number of arguments."
-    echo "There must be 2 args."
-    echo "1. First one is permission code"
-    echo "2. File/Folder with complete path"
+    echo "First arg must be number of length 3."
+    exit 1
 fi
 
 
